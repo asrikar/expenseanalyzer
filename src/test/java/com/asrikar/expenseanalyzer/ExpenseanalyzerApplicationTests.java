19eddaf996ca48e3b070f23e32353434a73d41ff
asrikar <ramaraosrikakulapu@gmail.com>
2021-02-06 22:43:23 -0500

    initial baseline

---
+++ src/test/java/com/asrikar/expenseanalyzer/ExpenseanalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asrikar.expenseanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseanalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
